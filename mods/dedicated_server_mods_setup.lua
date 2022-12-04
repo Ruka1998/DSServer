8d2b0dda8d7b839a707645daac868f598625ad48
Ruka1998 <ranncheng@gmail.com>
2022-12-04 18:14:09 +0800

    test

--- mods/dedicated_server_mods_setup.lua
+++ mods/dedicated_server_mods_setup.lua
@@ -12,6 +12,9 @@ ServerModSetup("666155465")
 ServerModSetup("2881739960")
 ServerModSetup("1301033176")
 
+
+--test 
+
 --ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
 	--The Workshop id can be found at the end of the url to the collection's Workshop page.
 	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
